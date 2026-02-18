d59ac6f8e9fe8842f3780226e4f3b4020965d90d
AbhinandanMishra-IT <mishra.ravi94@gmail.com>
2026-02-18 14:27:00 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,31 +1,33 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
-    header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.toggle("sticky", window.scrollY > 0);
 });
 
+// Mobile Menu
 let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-x');
-    navlist.classList.toggle('active');
+  menu.classList.toggle('bx-x');
+  navlist.classList.toggle('active');
 };
 
 window.onscroll = () => {
-    menu.classList.remove('bx-x');
-    navlist.classList.remove('active');
+  menu.classList.remove('bx-x');
+  navlist.classList.remove('active');
 };
 
+// Scroll Reveal Animations
 const sr = ScrollReveal({
-    distance: '50px',
-    duration: 2000,
-    reset: false
+  distance: '50px',
+  duration: 1500,
+  reset: false
 });
 
 sr.reveal('.home-text', { delay: 200, origin: 'left' });
-sr.reveal('.home-img', { delay: 200, origin: 'right' });
+sr.reveal('.home-img', { delay: 300, origin: 'right' });
 sr.reveal('.about, .experience, .portfolio, .service, .contact', {
-    delay: 150,
-    origin: 'bottom'
+  delay: 200,
+  origin: 'bottom'
 });
