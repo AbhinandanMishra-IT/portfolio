0ca29021423c708ba1aad6843cf85aca9a2be1ea
AbhinandanMishra-IT <mishra.ravi94@gmail.com>
2026-02-21 16:24:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
-window.addEventListener("scroll", () => {
-  header.classList.toggle("sticky", window.scrollY > 0);
+window.addEventListener("scroll", function() {
+    header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 // Mobile Menu
